966f1154efad77c3883f1b431f829b0a3820f663
Y-v-a-i-n <107935034+Y-v-a-i-n@users.noreply.github.com>
2022-11-11 00:10:16 +0100

    convert

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # convert
- A simple algorithm in python to convert values in binary, octal, decimal, hexadecimal and BCD
+A simple algorithm in python to convert values in binary, octal, decimal, hexadecimal and BCD
---
+++ convert.py
@@ -0,0 +1,26 @@
+def prefixDel(number,prefix):
+    return str(number).split(prefix)[1]
+def convert(number):
+    BCD="";
+    for hihi in list(str(number)):
+        bae=prefixDel(bin(int(hihi)),"0b");
+        while len(bae)!=4:
+            bae="0"+bae;
+        BCD+=bae+" ";
+    return "binaire : "+prefixDel(bin(number),"0b")+" | octal : "+prefixDel(oct(number),"0o")+" | decimal : "+str(int(number))+" | hexadecimal : "+prefixDel(hex(number),"0x")+" | BCD : "+BCD;
+def BCD(number):
+    finalNumber="";
+    ifBCDValid=True;
+    for binary in str(number).split(" "):
+        toDec=eval("0b"+binary);
+        if toDec<=9 and toDec>=0 and ifBCDValid:
+            finalNumber+=str(toDec);
+        else:
+            ifBCDValid=False;
+    if ifBCDValid:
+        return convert(int(finalNumber));
+def count(N):
+    number = 0;
+    while number != N+1:
+        print(convert(number));
+        number+=1;
